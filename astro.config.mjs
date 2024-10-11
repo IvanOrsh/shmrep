6d13ecf514953aa90b09087f28ac24002440ad75
IvanOrsh <ksironov@mail.ru>
2024-10-12 02:27:10 +0300

    chore: add solid-js

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,9 @@ import tailwind from '@astrojs/tailwind';
 
 import partytown from '@astrojs/partytown';
 
+import solidJs from '@astrojs/solid-js';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), partytown()]
+  integrations: [tailwind(), partytown(), solidJs()]
 });
